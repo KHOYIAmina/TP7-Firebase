4941ecec88ba5781c189ddc41a8fe866ba7dab47
amina <khoyii.amina@gmail.com>
2024-12-27 12:45:30 +0100

    2 commit

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app/home/homepage2.dart';
+import 'package:app/home/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
